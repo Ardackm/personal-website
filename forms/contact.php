f3427d1d954172118277a418eadca89b987d6072
Muhammet Arda <mardacakmak07@gmail.com>
2022-08-06 12:23:18 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
  
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'mardacakmak@outlook.com';
+  $receiving_email_address = 'info@ardackm.site';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
